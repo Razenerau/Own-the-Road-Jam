e23aaaa66b98c329a5a772cfa41e35b58ca5d370
Razenerau <molokov.phil@gmail.com>
2026-01-24 15:30:06 -0500

    added slines and cars

--- Assets/CarNPCController.cs
+++ Assets/CarNPCController.cs
@@ -14,8 +14,16 @@ public class CarNPCController : MonoBehaviour
         _oldPosition = transform.position;
         transform.position = DollyCart.transform.position;
 
+        
+
         float deltaX = transform.position.x - _oldPosition.x;
         float deltaY = transform.position.y - _oldPosition.y;
+
+        if(deltaX == 0)
+        {
+            transform.eulerAngles = Vector3.zero;
+            return;
+        }
 
         float angle = Mathf.Atan2(deltaY, deltaX);
         transform.eulerAngles = new Vector3(0, 0, angle * Mathf.Rad2Deg - 90);
