c216bdc53206c2eccb728b2b0a591a074bd82178
Razenerau <molokov.phil@gmail.com>
2026-03-23 20:56:28 -0400

    added animatiion to menu buttons and more SFX

--- Assets/CollisionController.cs
+++ Assets/CollisionController.cs
@@ -9,23 +9,43 @@ public class CollisionController : MonoBehaviour
 
     [Header("SFX")]
     public List<AudioClip> HitPersonList;
+    public List<AudioClip> HitCarList;
     public AudioClip HitWall;
 
     public static CollisionController Instance;
 
     private void Awake()
     {
+        peopleInjured = 0;
+
+        if (Instance == null)
         Instance = this;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "People" || collision.gameObject.tag == "Cars")
+        if(collision.gameObject.tag == "People")
         {
             collision.gameObject.SetActive(false);
             peopleInjured++;
 
             SFXManager.Instance.PlaySFX(HitPersonList, transform, 1f);
+        }
+
+        if(collision.gameObject.tag == "Cars")
+        {
+            collision.gameObject.SetActive(false);
+
+            int pplInjured = Random.Range(1, 4);
+
+            peopleInjured += pplInjured;
+
+            SFXManager.Instance.PlaySFX(HitCarList, transform, 0.7f);
+        }
+
+        if (collision.gameObject.tag == "Walls")
+        {
+            SFXManager.Instance.PlaySFX(HitWall, transform, 1f);
         }
     }
 }
